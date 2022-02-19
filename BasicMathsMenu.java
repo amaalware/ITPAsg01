005ac27629a776e3f21a4063f70f219ee9812b8f
amaal <98730239+amaalware@users.noreply.github.com>
2022-02-19 11:17:05 +0800

    Add files via upload
    + VALIDATION FOR ANSWERS
    + MULTIPLICATION
    + SUBTRACTION

--- BasicMathsMenu.java
+++ BasicMathsMenu.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
 public class BasicMathsMenu {
-
 	public static void main(String[] args) {
 
 		int menuChoice = 0;
 
-		
+		// MENU (AMAAL)
 		System.out.println("Welcome to Basic Maths Practice for Year 1! \r \n"
 				+ "1. Addition (+) \r\n" 
 				+ "2. Subtraction (-) \r\n"
@@ -49,9 +48,73 @@ public class BasicMathsMenu {
 		
 	// ADDITION - NAJIBAH'S PART
 		public static void getAddition()  {  
-			System.out.println("addition");
-	}
-	
+			
+			int addChoice = 0;
+			
+			
+			System.out.println("\n---------------------------- ");
+			System.out.println(" \n Addition Practice"
+								+ "\r \n You can practice LEVEL 1 or LEVEL 2"
+								+ "\n   Input 1 for LEVEL 1"
+								+ "\n   Input 2 for LEVEL 2");
+			System.out.print("\n---------------------------- \r \n");
+
+			addChoice = errorChecklevel(); // error Check for level
+			
+			if (addChoice == 1) {
+				int RandNo1= 0;
+				int RandNo2= 0;
+				int y = 0; //initial correct answer = 0
+				int n = 0; // initial wrong answer = 0
+				for (int i=1;i<11;i++) { //loop to generate 10 questions
+				RandNo1 = randomNo1digit();
+				RandNo2 = randomNo1digit();
+				System.out.printf("Question %d: What is %d + %d ? = ", i, RandNo1,RandNo2);
+				Scanner ans = new Scanner(System.in);
+				int answer = errorCheckAnswer();
+				int Correct = RandNo1 + RandNo2;
+				if (answer == Correct) {
+					y++;
+				}
+				else {
+					n++;
+				}
+				}
+				System.out.println("Total correct answer(s):"+ y);
+				System.out.println("Total incorrect answer(s):"+ n);
+				System.out.println("Questions that are answered correctly:");
+				System.out.println("Questions that are answered incorrectly:");
+				return;
+				}
+			
+			else if (addChoice == 2) {
+				int RandNo1= 0;
+				int RandNo2= 0;
+				int y = 0; //initial correct answer = 0
+				int n = 0; // initial wrong answer = 0
+				for (int i=1;i<11;i++) { //loop to generate 10 questions
+					RandNo1 = randomNo2digit();
+					RandNo2 = randomNo2digit();
+					System.out.printf("Question %d: What is %d + %d ? = ", i, RandNo1,RandNo2);
+					Scanner ans = new Scanner(System.in);
+					int answer = ans.nextInt();
+					int Correct = RandNo1 + RandNo2;
+					if (answer == Correct) {
+						y++;
+					}
+					else {
+						n++;
+					}
+					}
+					System.out.println("Total correct answer(s):"+ y);
+					System.out.println("Total incorrect answer(s):"+ n);
+					System.out.println("Questions that are answered correctly:");
+					System.out.println("Questions that are answered incorrectly:");
+					return;
+					}
+		}
+		
+		
 	// SUBTRACTION - AMAAL'S PART
 		public static void getSubtraction()  {  
 			
@@ -65,50 +128,227 @@ public class BasicMathsMenu {
 								+ "\n   Input 2 for LEVEL 2");
 			System.out.println("\n---------------------------- \r \n");
 			
-		
-			subChoice = errorCheck();
+			subChoice = errorChecklevel(); 
 			
-						}
-	
+			if (subChoice == 1) {
+				int RandNo1= 0;
+				int RandNo2= 0;
+				int y = 0; //initial correct answer = 0
+				int n = 0; // initial wrong answer = 0
+				for (int i=1;i<11;i++) { //loop to generate 10 questions
+				RandNo1 = randomNo1digit();
+				RandNo2 = randomNo1digit();
+				System.out.printf("Question %d: What is %d - %d ? = ", i, RandNo1,RandNo2);
+				Scanner ans = new Scanner(System.in);
+				int answer = errorCheckAnswer();
+				int Correct = RandNo1 - RandNo2;
+				if (answer == Correct) {
+					y++;
+				}
+				else {
+					n++;
+				}
+				}
+				System.out.println("Total correct answer(s):"+ y);
+				System.out.println("Total incorrect answer(s):"+ n);
+				System.out.println("Questions that are answered correctly:");
+				System.out.println("Questions that are answered incorrectly:");
+				return;
+				}
+			
+			else if (subChoice == 2) {
+				int RandNo1= 0;
+				int RandNo2= 0;
+				int y = 0; //initial correct answer = 0
+				int n = 0; // initial wrong answer = 0
+				for (int i=1;i<11;i++) { //loop to generate 10 questions
+					RandNo1 = randomNo2digit();
+					RandNo2 = randomNo2digit();
+					System.out.printf("Question %d: What is %d - %d ? = ", i, RandNo1,RandNo2);
+					Scanner ans = new Scanner(System.in);
+					int answer = ans.nextInt();
+					int Correct = RandNo1 - RandNo2;
+					if (answer == Correct) {
+						y++;
+					}
+					else {
+						n++;
+					}
+					}
+					System.out.println("Total correct answer(s):"+ y);
+					System.out.println("Total incorrect answer(s):"+ n);
+					System.out.println("Questions that are answered correctly:");
+					System.out.println("Questions that are answered incorrectly:");
+					return;
+					}
+			}
+
 		
+			
 	// MULTIPLICATION - AMAAL'S PART
-				public static void getMultiplication()  {  
-					System.out.println("MULTIPLICATION");
+	public static void getMultiplication()  {  
+		
+		int mulChoice = 0;
+		
+		
+		System.out.println("\n---------------------------- ");
+		System.out.println(" \n Subtraction Practice"
+							+ "\r \n You can practice LEVEL 1 or LEVEL 2"
+							+ "\n   Input 1 for LEVEL 1"
+							+ "\n   Input 2 for LEVEL 2");
+		System.out.println("\n---------------------------- \r \n");
+		
+		mulChoice = errorChecklevel(); 
+		
+		if (mulChoice == 1) {
+			int RandNo1= 0;
+			int RandNo2= 0;
+			int y = 0; //initial correct answer = 0
+			int n = 0; // initial wrong answer = 0
+			for (int i=1;i<11;i++) { //loop to generate 10 questions
+			RandNo1 = randomNo1digit();
+			RandNo2 = randomNo1digit();
+			System.out.printf("Question %d: What is %d * %d ? = ", i, RandNo1,RandNo2);
+			Scanner ans = new Scanner(System.in);
+			int answer = errorCheckAnswer();
+			int Correct = RandNo1 * RandNo2;
+			if (answer == Correct) {
+				y++;
 			}
-			
-	// DIVISION - NADYAH'S PART
-				public static void getDivision()  {  
-					System.out.println("DIVISION");
+			else {
+				n++;
 			}
-
-
-
-				public static int errorCheck(){
-
-					String strInput;
-					boolean valid = false;
-					int choice = 0;
-
-					Scanner in = new Scanner(System.in);
-
-					do {
-						strInput = in.nextLine();
-						valid = false;
-
-						try {
-							choice = Integer.parseInt(strInput);
-
-							if (choice < 1 || choice > 5) {
-								System.out.println("Error - please only input 1-5 only.");
-								valid = true;
-							}
-
-						}
-						catch (NumberFormatException e) {
-							System.out.println("Error - please only input a whole number.");
-							valid = true;
-						} 
-					} while(valid==true);
-					return choice;
+			}
+			System.out.println("Total correct answer(s):"+ y);
+			System.out.println("Total incorrect answer(s):"+ n);
+			System.out.println("Questions that are answered correctly:");
+			System.out.println("Questions that are answered incorrectly:");
+			return;
+			}
+		
+		else if (mulChoice == 2) {
+			int RandNo1= 0;
+			int RandNo2= 0;
+			int y = 0; //initial correct answer = 0
+			int n = 0; // initial wrong answer = 0
+			for (int i=1;i<11;i++) { //loop to generate 10 questions
+				RandNo1 = randomNo2digit();
+				RandNo2 = randomNo2digit();
+				System.out.printf("Question %d: What is %d * %d ? = ", i, RandNo1,RandNo2);
+				Scanner ans = new Scanner(System.in);
+				int answer = ans.nextInt();
+				int Correct = RandNo1 * RandNo2;
+				if (answer == Correct) {
+					y++;
+				}
+				else {
+					n++;
+				}
+				}
+				System.out.println("Total correct answer(s):"+ y);
+				System.out.println("Total incorrect answer(s):"+ n);
+				System.out.println("Questions that are answered correctly:");
+				System.out.println("Questions that are answered incorrectly:");
+				return;
 				}
 			}
+	
+			
+	// DIVISION - NADYAH'S PART
+	public static void getDivision()  {  
+			System.out.println("DIVISION");
+			}
+
+
+	
+	// RANDOM NUMBERS GENERATOR (NAJIBAH)
+	
+	public static int randomNo1digit() { // Random numbers for Level 1
+			int min = 0;
+			int max = 9;
+			int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
+			return random_int;
+		}
+	
+	public static int randomNo2digit() { // Random numbers for Level 2
+			int min = 10;
+			int max = 99;
+			int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
+			return random_int;
+		}
+	
+	
+	// VALIDATION FOR MAIN MENU (AMAAL)
+	public static int errorCheck(){ 
+		String strInput;
+		boolean valid = false;
+		int choice = 0;
+		Scanner in = new Scanner(System.in);
+		do {
+			strInput = in.nextLine();
+			valid = false;
+
+			try {
+				choice = Integer.parseInt(strInput);
+
+			if (choice < 1 || choice > 5) {
+			System.out.println("Error - please only input 1-5 only.");
+			valid = true;
+			}
+			}
+			catch (NumberFormatException e) {
+			System.out.println("Error - please only input a whole number.");
+			valid = true;
+			} 
+		} while(valid==true);
+		return choice;
+		}
+	
+	// VALIDATION FOR LEVEL (NAJIBAH)
+	public static int errorChecklevel(){ // error check for Level
+		String strInput;
+		boolean valid = false;
+		int choice = 0;
+		Scanner in = new Scanner(System.in);
+		do {
+			strInput = in.nextLine();
+			valid = false;
+
+			try {
+				choice = Integer.parseInt(strInput);
+
+			if (choice < 1 || choice > 2) {
+			System.out.println("Error - please only input the levels available only.");
+			valid = true;
+			}
+			}
+			catch (NumberFormatException e) {
+			System.out.println("Error - please only input a whole number.");
+			valid = true;
+			} 
+		} while(valid==true);
+		return choice;
+		}
+	
+
+// VALIDATION FOR ANSWERS (AMAAL)
+public static int errorCheckAnswer(){ 
+	String strInput;
+	boolean valid = false;
+	int choice = 0;
+	Scanner in = new Scanner(System.in);
+	do {
+		strInput = in.nextLine();
+		valid = false;
+
+		try {
+			choice = Integer.parseInt(strInput);
+		}
+		catch (NumberFormatException e) {
+		System.out.println("Error - please only input a whole number.");
+		valid = true;
+		} 
+	} while(valid==true);
+	return choice;
+	}
+}
