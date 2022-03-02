f30fcca75722204b7fb3cd2e66f31ecb7a7e5bea
Jeebsy23 <98743706+Jeebsy23@users.noreply.github.com>
2022-03-02 20:12:19 +0800

    Update BasicMathsMenu.java
    + Addition
    + Division
    + Random Numbers (double)

--- BasicMathsMenu.java
+++ BasicMathsMenu.java
@@ -64,58 +64,115 @@ public class BasicMathsMenu {
 			addChoice = errorChecklevel(); // error Check for level
 			
 			if (addChoice == 1) {
+				
 				int RandNo1= 0;
 				int RandNo2= 0;
 				int y = 0; //initial correct answer = 0
 				int n = 0; // initial wrong answer = 0
-				for (int i=1;i<11;i++) { //loop to generate 10 questions
+				int i = 0;
+				int j = 0;
+				int wrongQnNumbers[] = new int[11];
+				int correctQnNumbers[] = new int[11];
+				int wrongQnAns[] = new int[11];
+				
+				for (i=1;i<11;i++) { //loop to generate 10 questions
+				
 				RandNo1 = randomNo1digit();
 				RandNo2 = randomNo1digit();
+				
 				System.out.printf("Question %d: What is %d + %d ? = ", i, RandNo1,RandNo2);
 				Scanner ans = new Scanner(System.in);
+				
 				int answer = errorCheckAnswer();
 				int Correct = RandNo1 + RandNo2;
+				
 				if (answer == Correct) {
 					y++;
+					correctQnNumbers[y] = i;
 				}
 				else {
 					n++;
+					wrongQnNumbers[n] = i;
+					wrongQnAns[n] = Correct;
 				}
+			}
+				
+				System.out.println("\n===========================");
+				System.out.println("\n RESULTS");
+				System.out.println("\n===========================");
+				System.out.println("\n Total correct answer(s):"+ y);
+				System.out.println("\n Total incorrect answer(s):"+ n);
+				
+				System.out.println("\n Below are the questions that are answered correctly:");
+				
+				for (j= 0; j < y; j++) {
+					System.out.print("\n Question " + correctQnNumbers[j+1] + ". " );
 				}
-				System.out.println("Total correct answer(s):"+ y);
-				System.out.println("Total incorrect answer(s):"+ n);
-				System.out.println("Questions that are answered correctly:");
-				System.out.println("Questions that are answered incorrectly:");
-				return;
+				
+
+				System.out.println("\r \n Below are the questions that are answered incorrectly:" );
+				
+				for (j= 0; j < n; j++) {
+					System.out.print("\n Question " + wrongQnNumbers[j+1] + "; The correct answer is " + wrongQnAns[j+1] );
 				}
+			}
 			
 			else if (addChoice == 2) {
+				
 				int RandNo1= 0;
 				int RandNo2= 0;
 				int y = 0; //initial correct answer = 0
 				int n = 0; // initial wrong answer = 0
-				for (int i=1;i<11;i++) { //loop to generate 10 questions
-					RandNo1 = randomNo2digit();
-					RandNo2 = randomNo2digit();
-					System.out.printf("Question %d: What is %d + %d ? = ", i, RandNo1,RandNo2);
-					Scanner ans = new Scanner(System.in);
-					int answer = ans.nextInt();
-					int Correct = RandNo1 + RandNo2;
-					if (answer == Correct) {
-						y++;
-					}
-					else {
-						n++;
-					}
-					}
-					System.out.println("Total correct answer(s):"+ y);
-					System.out.println("Total incorrect answer(s):"+ n);
-					System.out.println("Questions that are answered correctly:");
-					System.out.println("Questions that are answered incorrectly:");
+				int i = 0;
+				int j = 0;
+				int wrongQnNumbers[] = new int[11];
+				int correctQnNumbers[] = new int[11];
+				int wrongQnAns[] = new int[11];
+				
+				for (i=1;i<11;i++) { //loop to generate 10 questions
+				
+				RandNo1 = randomNo2digit();
+				RandNo2 = randomNo2digit();
+				
+				System.out.printf("Question %d: What is %d + %d ? = ", i, RandNo1,RandNo2);
+				Scanner ans = new Scanner(System.in);
+				
+				int answer = errorCheckAnswer();
+				int Correct = RandNo1 + RandNo2;
+				
+				if (answer == Correct) {
+					y++;
+					correctQnNumbers[y] = i;
+				}
+				else {
+					n++;
+					wrongQnNumbers[n] = i;
+					wrongQnAns[n] = Correct;
+				}
+				}
+				System.out.println("\n===========================");
+				System.out.println("\n RESULTS");
+				System.out.println("\n===========================");
+				System.out.println("\n Total correct answer(s):"+ y);
+				System.out.println("\n Total incorrect answer(s):"+ n);
+				
+				System.out.println("\n Below are the questions that are answered correctly:");
+				
+				for (j= 0; j < y; j++) {
+					System.out.print("\n Question " + correctQnNumbers[j+1] + ". " );
+				}
+				
+
+				System.out.println("\r \n Below are the questions that are answered incorrectly:" );
+				
+				for (j= 0; j < n; j++) {
+					System.out.print("\n Question " + wrongQnNumbers[j+1] + "; The correct answer is " + wrongQnAns[j+1] );
+				}	
 					return;
-					}
+			}
 		}
-		
+	
+	
 		
 	// SUBTRACTION - AMAAL'S PART
 		public static void getSubtraction()  {  
@@ -383,66 +440,135 @@ public class BasicMathsMenu {
 	public static void getDivision()  { 
 		
 		int DivChoice = 0;
-			
-			
+		
+		
 		System.out.println("\n---------------------------- ");
-		System.out.println(" \n Division Practice"
+		System.out.println(" \n Multiplication Practice"
 							+ "\r \n You can practice LEVEL 1 or LEVEL 2"
 							+ "\n   Input 1 for LEVEL 1"
 							+ "\n   Input 2 for LEVEL 2");
-		System.out.print("\n---------------------------- \r \n");
+		System.out.println("\n---------------------------- \r \n");
+		
+		mulChoice = errorChecklevel(); 
+		
 
 		DivChoice = errorChecklevel();
+		if (DivChoice == 1) {
 			
-			if (DivChoice == 1) {
-				float RandNo1= 0;
-				float RandNo2= 0;
-				int y = 0;
-				int n = 0;
-				for (int i=1;i<11;i++) { 
-				RandNo1 = randomNo3digit();
-				RandNo2 = randomNo3digit();
-				System.out.printf("Question %d: What is %.1f / %.1f ? = ", i, RandNo1,RandNo2);
+			double RandNo3= 0;
+			double RandNo4= 0;
+			int y = 0;
+			int n = 0;
+			int i =0;
+			int j =0;
+			double wrongQnNumbers[] = new double[11];
+			double correctQnNumbers[] = new double[11];
+			double wrongQnAns[] = new double[11];
+				
+			System.out.print("(Please enter your answer in 1 decimal place for long answer)\n");
+				
+			for (i=1 ; i < 11 ; i++) { 
+				
+			RandNo3 = randomNo3digit();
+			RandNo4 = randomNo3digit();
+					
+			System.out.printf("Question %d: What is %.1f / %.1f ? = ", i, RandNo3,RandNo4);
+			Scanner ans = new Scanner(System.in);
+					
+			double answer = ans.nextDouble();
+			double Correct = RandNo3 / RandNo4; 
+			double roundOff = Math.round(Correct*10)/10.0; //convert into 1 dp
+
+			if (answer == roundOff) { // accept correct answer that are in 1 dp 
+				y++;
+				correctQnNumbers[y] = i;
+				
+			}
+			else {
+				n++;
+				wrongQnNumbers[n] = i;
+				wrongQnAns[n] = roundOff;
+			}
+			}
+				
+			System.out.println("\n===========================");
+			System.out.println("\n RESULTS");
+			System.out.println("\n===========================");
+			System.out.println("\n Total correct answer(s):"+ y);
+			System.out.println("\n Total incorrect answer(s):"+ n);
+				
+			System.out.println("\n Below are the questions that are answered correctly:");
+				
+			for (j= 0; j < y; j++) {
+				System.out.print("\n Question " + correctQnNumbers[j+1] + ". " );
+			}
+				
+
+			System.out.println("\r \n Below are the questions that are answered incorrectly:" );
+				
+			for (j= 0; j < n; j++) {
+				System.out.print("\n Question " + wrongQnNumbers[j+1] + "; The correct answer is " + wrongQnAns[j+1] );
+			}
+				
+			return;
+			}
+			
+		else if (DivChoice == 2) {
+			double RandNo3= 0;
+			double RandNo4= 0;
+				
+			int y = 0;
+			int n = 0;
+			int i =0;
+			int j =0;
+			double wrongQnNumbers[] = new double[11];
+			double correctQnNumbers[] = new double[11];
+			double wrongQnAns[] = new double[11];
+				
+			System.out.print("(Please enter your answer in 1 decimal place for long answer)\n");
+				
+			for (i=1 ; i < 11 ; i++) { 
+					
+				RandNo3 = randomNo4digit();
+				RandNo4 = randomNo4digit();
+					
+				System.out.printf("Question %d: What is %.1f / %.1f ? = ", i, RandNo3,RandNo4);
 				Scanner ans = new Scanner(System.in);
-				float answer = errorCheckAnswer();
-				float Correct = RandNo1 / RandNo2;
-				if (answer == Correct) {
+					
+				double answer = ans.nextDouble();
+				double Correct = RandNo3 / RandNo4; 
+				double roundOff = Math.round(Correct*10)/10.0; //convert into 1 dp
+				
+				if (answer == roundOff) { // accept correct answer that are in 1 dp 
 					y++;
+					correctQnNumbers[y] = i;
 				}
 				else {
 					n++;
+					wrongQnNumbers[n] = i;
+					wrongQnAns[n] = roundOff;
 				}
 				}
-				System.out.println("Total correct answer(s):"+ y);
-				System.out.println("Total incorrect answer(s):"+ n);
-				System.out.println("Questions that are answered correctly:");
-				System.out.println("Questions that are answered incorrectly:");
-				return;
-				}
-			
-			else if (DivChoice == 2) {
-				float RandNo1= 0;
-				float RandNo2= 0;
-				int y = 0;
-				int n = 0; 
-				for (int i=1;i<11;i++) { 
-				RandNo1 = randomNo4digit();
-				RandNo2 = randomNo4digit();
-				System.out.printf("Question %d: What is %.1f / %.1f ? = ", i, RandNo1,RandNo2);
-				Scanner ans = new Scanner(System.in);
-				float answer = ans.nextFloat();
-				float Correct = RandNo1 / RandNo2;
-				if (answer == Correct) {
-					y++;
-				}
-				else {
-					n++;
-				}
-				}
-				System.out.println("Total correct answer(s):"+ y);
-				System.out.println("Total incorrect answer(s):"+ n);
-				System.out.println("Questions that are answered correctly:");
-				System.out.println("Questions that are answered incorrectly:");
+				
+			System.out.println("\n===========================");
+			System.out.println("\n RESULTS");
+			System.out.println("\n===========================");
+			System.out.println("\n Total correct answer(s):"+ y);
+			System.out.println("\n Total incorrect answer(s):"+ n);
+				
+			System.out.println("\n Below are the questions that are answered correctly:");
+				
+			for (j= 0; j < y; j++) {
+				System.out.print("\n Question " + correctQnNumbers[j+1] + ". " );
+			}
+				
+
+			System.out.println("\r \n Below are the questions that are answered incorrectly:" );
+				
+			for (j= 0; j < n; j++) {
+				System.out.print("\n Question " + wrongQnNumbers[j+1] + "; The correct answer is " + wrongQnAns[j+1] );
+			}
+				
 				return;
 				}
 	}
@@ -451,33 +577,32 @@ public class BasicMathsMenu {
 	
 	// RANDOM NUMBERS GENERATOR (NAJIBAH & NADYAH)
 	public static int randomNo1digit() { // Random numbers for Level 1
-		 
-			int min = 0;
-			int max = 9;
-			int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
-			return random_int;
-		}
+		 int min = 0;
+		 int max = 9;
+		 int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
+		 return random_int;
+	}
 	
 	public static int randomNo2digit() { // Random numbers for Level 2
-			int min = 10;
-			int max = 99;
-			int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
-			return random_int;
-		}
+		int min = 10;
+		int max = 99;
+		int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
+		return random_int;
+	}
 	
-	public static float randomNo3digit() {// Random numbers for level 1 (division)
-		float min = 1;
-		float max = 9;
-		float random_float = (int)Math.floor(Math.random()*(max-min+1)+min);
-		return random_float;
-	       }
-	
-	public static float randomNo4digit() {// Random numbers for level 2 (division)
-		float min = 10;
-		float max = 99;
-		float random_float = (int)Math.floor(Math.random()*(max-min+1)+min);
-		return random_float;
-	        }
+	public static double randomNo3digit() {// Random numbers for level 1 (division)
+		double min = 1;
+		double max = 9;
+		double random_double = (int)Math.floor(Math.random()*(max-min+1)+min);
+		return random_double;
+	}
+
+	public static double randomNo4digit() {// Random numbers for level 2 (division)
+		double min = 10;
+		double max = 99;
+		double random_double = (int)Math.floor(Math.random()*(max-min+1)+min);
+		return random_double;
+	}
 	
 	
 	// VALIDATION FOR MAIN MENU (AMAAL)
